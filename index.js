89d3b3641e8b7c2a4735837eb369b8aed80b270b
batraz <baticpro@gmail.com>
2020-03-27 11:47:08 +0300

    run testing classify

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tfnode from '@tensorflow/tfjs-node';
+import * as fs from 'fs';
+
+const loadAndDecodeImage = path => {
+  const buffer = fs.readFileSync(path);
+  return tfnode.node.decodeImage(buffer);
+};
+
+const getPredictions = async path => {
+  const image = loadAndDecodeImage(path);
+  const model = await mobilenet.load();
+  return await model.classify(image);
+};
 
 const main = async () => {
-  console.log('test')
+  const predictions = await getPredictions('testing-images/IMG_5313.JPG');
+  console.log('results: ', predictions);
 };
 
 main();
